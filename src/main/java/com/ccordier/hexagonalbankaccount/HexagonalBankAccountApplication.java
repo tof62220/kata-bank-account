c06dac04a52c6d534e5abbe0973252dba2465916
Christophe Cordier <c.cordier@oodrive.com>
2022-09-15 10:33:23 +0200

    Initialize Spring Boot Project

---
+++ src/main/java/com/ccordier/hexagonalbankaccount/HexagonalBankAccountApplication.java
@@ -0,0 +1,13 @@
+package com.ccordier.hexagonalbankaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalBankAccountApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalBankAccountApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ccordier/hexagonalbankaccount/HexagonalBankAccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccordier.hexagonalbankaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalBankAccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
