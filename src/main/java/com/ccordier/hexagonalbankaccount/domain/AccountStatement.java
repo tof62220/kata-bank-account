0bbd2516ca3a9313883859ec14b89b971032347f
Christophe Cordier <c.cordier@oodrive.com>
2022-09-18 14:13:36 +0200

    Add AccountStatement Class

---
+++ src/main/java/com/ccordier/hexagonalbankaccount/domain/AccountStatement.java
@@ -0,0 +1,52 @@
+package com.ccordier.hexagonalbankaccount.domain;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import lombok.EqualsAndHashCode;
+
+
+/**
+ * The account statement on a bank account
+ * 
+ * @author c.cordier
+ */
+@EqualsAndHashCode
+public class AccountStatement {
+
+	private final List<Operation> operations;
+
+	/**
+	 * Constructs an account statement with a list of operations on a bank account
+	 * 
+	 * @param operations The list of operations on a bank account
+	 */
+	public AccountStatement(final List<Operation> operations) {
+		Objects.requireNonNull(operations, "List of operations is mandatory.");
+		this.operations = List.copyOf(operations);
+	}
+
+	/**
+	 * Returns a new {@code AccountStatement} whose operations are existing operations from {@code (this)} and the new
+	 * one from method parameter
+	 * 
+	 * @param operation The operation to be added to this {@code AccountStatement}
+	 * @return a new {@code AccountStatement} with old operations and the new one
+	 */
+	public AccountStatement addOperation(final Operation operation) {
+		final List<Operation> copyOfOperations = operations.stream().collect(Collectors.toList());
+		copyOfOperations.add(operation);
+		return new AccountStatement(copyOfOperations);
+	}
+
+	/**
+	 * Returns the list of operations sorted by date on this {@code AccountStatement}
+	 * 
+	 * @return The list of operations sorted by date
+	 */
+	public List<Operation> getOperations() {
+		return operations.stream().sorted().collect(Collectors.toUnmodifiableList());
+	}
+
+}
