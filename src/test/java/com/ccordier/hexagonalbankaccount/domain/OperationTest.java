884088a818f168ea16b0cfebf42097b003c9a207
Christophe Cordier <c.cordier@oodrive.com>
2022-09-18 11:44:35 +0200

    Operation Class Unit Test

---
+++ src/test/java/com/ccordier/hexagonalbankaccount/domain/OperationTest.java
@@ -0,0 +1,156 @@
+package com.ccordier.hexagonalbankaccount.domain;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.ThrowingSupplier;
+
+import com.mongodb.client.model.changestream.OperationType;
+
+
+class OperationTest {
+
+	@Test
+	void testConstructorWithNotNullParameters() throws Exception {
+
+		OffsetDateTime oprationDate = OffsetDateTime.now();
+
+		Operation operation = assertDoesNotThrow(new ThrowingSupplier<Operation>() {
+			@Override
+			public Operation get() throws Throwable {
+				return new Operation(oprationDate,
+						OperationType.WITHDRAWAL,
+						PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+						PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+			}
+		});
+
+		assertNotNull(operation);
+		assertEquals(oprationDate, operation.getDate());
+		assertEquals(OperationType.WITHDRAWAL, operation.getType());
+		assertEquals(PositiveAmount.valueOf(BigDecimal.valueOf(500)), operation.getAmount());
+		assertEquals(PositiveAmount.valueOf(BigDecimal.valueOf(1500)), operation.getBalanceAfterOperation());
+
+	}
+
+	@Test
+	void testConstructorWithoutDate() throws Exception {
+
+		NullPointerException exception = assertThrows(NullPointerException.class,
+				() -> new Operation(null,
+						OperationType.WITHDRAWAL,
+						PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+						PositiveAmount.valueOf(BigDecimal.valueOf(1500))));
+
+		assertEquals("Client Number is mandatory.", exception.getMessage());
+
+	}
+
+	@Test
+	void testConstructorWithoutType() throws Exception {
+
+		NullPointerException exception = assertThrows(NullPointerException.class,
+				() -> new Operation(OffsetDateTime.now(),
+						null,
+						PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+						PositiveAmount.valueOf(BigDecimal.valueOf(1500))));
+
+		assertEquals("Client Number is mandatory.", exception.getMessage());
+
+	}
+
+	@Test
+	void testConstructorWithoutAmount() throws Exception {
+
+		NullPointerException exception = assertThrows(NullPointerException.class,
+				() -> new Operation(OffsetDateTime.now(),
+						OperationType.WITHDRAWAL,
+						null,
+						PositiveAmount.valueOf(BigDecimal.valueOf(1500))));
+
+		assertEquals("Client Number is mandatory.", exception.getMessage());
+
+	}
+
+	@Test
+	void testConstructorWithoutBalance() throws Exception {
+
+		NullPointerException exception = assertThrows(NullPointerException.class,
+				() -> new Operation(OffsetDateTime.now(),
+						OperationType.WITHDRAWAL,
+						PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+						null));
+
+		assertEquals("Client Number is mandatory.", exception.getMessage());
+
+	}
+
+	@Test
+	void testHashCodeWithSameOperation() throws Exception {
+		OffsetDateTime oprationDate = OffsetDateTime.now();
+		Operation opreration1 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+		Operation opreration2 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+
+		assertTrue(opreration1.hashCode() == opreration2.hashCode());
+	}
+
+	@Test
+	void testEqualsWithSameOperation() throws Exception {
+		OffsetDateTime oprationDate = OffsetDateTime.now();
+		Operation opreration1 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+		Operation opreration2 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+
+		assertTrue(opreration1.equals(opreration2));
+	}
+
+	@Test
+	void testHashCodeWithNotSameOperation() throws Exception {
+		OffsetDateTime oprationDate = OffsetDateTime.now();
+		Operation opreration1 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+		Operation opreration2 = new Operation(oprationDate,
+				OperationType.DEPOSIT,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+
+		assertFalse(opreration1.hashCode() == opreration2.hashCode());
+	}
+
+	@Test
+	void testEqualsWithNotSameOperation() throws Exception {
+		OffsetDateTime oprationDate = OffsetDateTime.now();
+		Operation opreration1 = new Operation(oprationDate,
+				OperationType.WITHDRAWAL,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+		Operation opreration2 = new Operation(oprationDate,
+				OperationType.DEPOSIT,
+				PositiveAmount.valueOf(BigDecimal.valueOf(500)),
+				PositiveAmount.valueOf(BigDecimal.valueOf(1500)));
+
+		assertFalse(opreration1.equals(opreration2));
+	}
+
+}
