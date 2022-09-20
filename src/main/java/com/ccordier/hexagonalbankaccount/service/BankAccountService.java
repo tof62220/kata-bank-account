a8757c46a92e7c5ebff659210e11b78d591c6e8b
Christophe Cordier <c.cordier@oodrive.com>
2022-09-20 11:52:21 +0200

    Add Some logs in BankAccountService

--- src/main/java/com/ccordier/hexagonalbankaccount/service/BankAccountService.java
+++ src/main/java/com/ccordier/hexagonalbankaccount/service/BankAccountService.java
@@ -14,12 +14,15 @@ import com.ccordier.hexagonalbankaccount.port.incoming.WithdrawUseCase;
 import com.ccordier.hexagonalbankaccount.port.outgoing.LoadAccountPort;
 import com.ccordier.hexagonalbankaccount.port.outgoing.SaveAccountPort;
 
+import lombok.extern.slf4j.Slf4j;
+
 
 /**
  * Service to manage bank accounts
  * 
  * @author c.cordier
  */
+@Slf4j
 public class BankAccountService implements DepositUseCase, WithdrawUseCase, HistoryUseCase {
 
 	private LoadAccountPort loadAccountPort;
@@ -32,29 +35,34 @@ public class BankAccountService implements DepositUseCase, WithdrawUseCase, Hist
 
 	@Override
 	public void deposit(UUID id, BigDecimal amount) {
+		log.info("Try to deposit amount on bank account with id = {}", id);
 		BankAccount account = loadAccountPort.load(id)
 				.orElseThrow(NoSuchElementException::new);
-
+		log.debug("Bank account with id = {} was found", id);
 		BankAccount updatedAccount = account.deposit(PositiveAmount.valueOf(amount));
-
+		log.debug("Deposit on bank account with id = {} was realized", id);
 		saveAccountPort.save(updatedAccount);
+		log.info("Bank account with id = {} was saved after deposit", id);
 	}
 
 	@Override
 	public void withdraw(UUID id, BigDecimal amount) {
+		log.info("Try to withdraw amount on bank account with id = {}", id);
 		BankAccount account = loadAccountPort.load(id)
 				.orElseThrow(NoSuchElementException::new);
-
+		log.debug("Bank account with id = {} was found", id);
 		BankAccount updatedAccount = account.withdraw(PositiveAmount.valueOf(amount));
-
+		log.debug("Withdrawal on bank account with id = {} was realized", id);
 		saveAccountPort.save(updatedAccount);
+		log.info("Bank account with id = {} was saved after withdrawal", id);
 	}
 
 	@Override
 	public List<Operation> history(UUID id) {
+		log.info("Try to find bank account with id = {}", id);
 		BankAccount account = loadAccountPort.load(id)
 				.orElseThrow(NoSuchElementException::new);
-
+		log.info("Bank account with id = {} was found", id);
 		return account.getHistory();
 	}
 
